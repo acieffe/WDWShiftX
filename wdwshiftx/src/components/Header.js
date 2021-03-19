b403b2ea0e3ae27fc457d305c8443534a08ce58e
Matthew Baugh <acieffe@gmail.com>
2021-03-18 22:14:43 -0400

    Fixed Header Style

--- wdwshiftx/src/components/Header.js
+++ wdwshiftx/src/components/Header.js
@@ -1,74 +1,69 @@
 import React from 'react';
-import styled from 'styled-components';
 import Logo from './Logo';
 import Login from './Login';
 import Register from './Register';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		width: '100%',
+		position: 'fixed',
+		top: '0px',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		backgroundColor: 'grey',
+		boxShadow: '0px 2px 5px',
+		zIndex: '40',
+	},
+	logoArea: {
+		marginLeft: '5px',
+	},
+	userContainer: {
+		display: 'flex',
+		alignItems: 'center',
+		paddingRight: '16px',
+	},
+	loginPrompt: {
+		display: 'flex',
+		paddingRight: '16px',
+	},
+	userName: {
+		display: 'flex',
+		paddingRight: '16px',
+	},
+	userImg: {
+		width: '28px',
+		height: '28px',
+		border: '2px solid white',
+		borderRadius: '3px',
+	},
+}));
 
 function Header() {
+	const classes = useStyles();
+
 	return (
-		<Container>
-			<LogoArea>
+		<div className={classes.container}>
+			<div className={classes.logoArea}>
 				<Logo />
-			</LogoArea>
-			<UserContainer>
+			</div>
+			<div className={classes.userContainer}>
 				{true ? (
-					<LoginPrompt>
+					<div className={classes.loginPrompt}>
 						<Login />
 						&nbsp;
 						<Register />
-					</LoginPrompt>
+					</div>
 				) : (
-					<UserName>Hello User</UserName>
+					<div className={classes.userName}>Hello User</div>
 				)}
-				<UserImg>
-					<img src="https://i.imgur.com/6VBx3io.png" alt="" />
-				</UserImg>
-			</UserContainer>
-		</Container>
+				<div className={classes.userImg}>
+					<img src="https://i.imgur.com/6VBx3io.png" alt="" width="100%" />
+				</div>
+			</div>
+		</div>
 	);
 }
 
 export default Header;
-
-const Container = styled.div`
-	width: 100%;
-	position: fixed;
-	top: 0px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	background-color: grey;
-	box-shadow: 0px 2px 5px;
-	z-index: 40;
-`;
-
-const LogoArea = styled.div`
-	margin-left: 5px;
-`;
-
-const UserContainer = styled.div`
-	display: flex;
-	align-items: center;
-	padding-right: 16px;
-`;
-
-const LoginPrompt = styled.div`
-	display: flex;
-	padding-right: 16px;
-`;
-
-const UserName = styled.div`
-	display: flex;
-	padding-right: 16px;
-`;
-
-const UserImg = styled.div`
-	width: 28px;
-	height: 28px;
-	border: 2px solid white;
-	border-radius: 3px;
-
-	img {
-		width: 100%;
-	}
-`;
